3cef33a37700f044c1899f9abee4a385b6d4a537
Howyoung <howyoung3@gmail.com>
2021-03-07 00:10:37 +0800

    Optimize todo list output

--- tronclass_cli/command/todo.py
+++ tronclass_cli/command/todo.py
@@ -1,19 +1,21 @@
-from tabulate import tabulate
-
 from tronclass_cli.command import Command
 from tronclass_cli.middleware.api import ApiMiddleware
+from tronclass_cli.middleware.table import TableMiddleware
 
 
 class TodoCommand(Command):
     name = 'todo'
-    middleware_classes = [ApiMiddleware]
+    middleware_classes = [ApiMiddleware, TableMiddleware]
 
     def _init_parser(self):
-        pass
+        default_fields = 'course_id,course_name,end_time,id,title,type'
+        self._parser.add_argument('--fields', default=default_fields,
+                                  help=f'fields to display, default fields: {default_fields}')
 
     def _exec(self, args):
+        fields = args.fields.split(',')
         todo = list(self._ctx.api.get_todo())
         if len(todo) == 0:
-            print('No tasks.')
+            print('No courses.')
         else:
-            print(tabulate(todo, headers='keys'))
+            self._ctx.print_table(todo, fields)
